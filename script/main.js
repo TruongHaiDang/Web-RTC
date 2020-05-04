3e511eb5fe229b76aabdc7b8f6596236d9f19154
Truong Hai Dang <haidanghth910@gmail.com>
2020-05-04 16:46:19 +0700

    ok

--- script/main.js
+++ script/main.js
@@ -31,8 +31,8 @@ function playStream(idVideoTag, stream) {
     video.play();
 }
 
-var peer = new Peer({key: 'peerjs', host: 'stream-video-rtc.herokuapp.com', secure: true, port: 443});
-// var peer = new Peer({key: 'peerjs'});
+// var peer = new Peer({key: 'peerjs', host: 'stream-video-rtc.herokuapp.com', secure: true, port: 443});
+var peer = new Peer({key: 'peerjs'});
 peer.on('open', id => {
     $('#nameLocalSection').append(`<h3>My ID: ${id}</h3>`);
     $('#btnSignUp').click(() => {
