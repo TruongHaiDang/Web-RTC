d76a933ce675a4b7e43db4d6bd7217119b09c9c9
Truong Hai Dang <haidanghth910@gmail.com>
2020-05-04 17:39:29 +0700

    haha

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
-const client = io("https://truonghaidang.github.io/Web-RTC/");
-// const client = io('http://localhost:443');
+// const client = io("https://truonghaidang.github.io/Web-RTC/");
+const client = io('https://localhost');
 const arrUsers = [];
 
 client.on('signup_notifications', (data) => {
