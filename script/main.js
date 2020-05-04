b3a8308c0da875c9f10a6ee04df0e5b893993690
Truong Hai Dang <haidanghth910@gmail.com>
2020-05-04 09:54:24 +0700

    ok

--- script/main.js
+++ script/main.js
@@ -9,5 +9,28 @@ function playStream(idVideoTag, stream) {
     video.play();
 }
 
-// openStream()
-// .then(stream => playStream('localStream', stream))
+var peer = new Peer();
+peer.on('open', id => $('#nameLocalSection').append(`<h3>My ID: ${id}</h3>`));
+
+$('document').ready(() => {
+    $('#btnCall').click(() => {
+        const id = $('#remoteId').val();
+        openStream()
+        .then(stream => {
+            playStream('localStream', stream);
+            const call = peer.call(id, stream);
+            call.on('stream', remoteStream => playStream('remoteStream', remoteStream))
+        }), (err) => {
+            console.log('Failed to get local stream' ,err);
+        }
+    })
+})
+
+peer.on('call', call => {
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream', stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    })
+})
