fcb874f4a864adc3a7a6d1180b17c1276099a99a
Truong Hai Dang <haidanghth910@gmail.com>
2020-05-05 14:05:46 +0700

    make it better

--- script/main.js
+++ script/main.js
@@ -32,7 +32,7 @@ function playStream(idVideoTag, stream) {
 }
 
 var peer = new Peer({host:'call-video-rtc.herokuapp.com', secure:true, port:35317});
-var peer = new Peer();
+// var peer = new Peer();
 peer.on('open', id => {
     $('#nameLocalSection').append(`<h3>My ID: ${id}</h3>`);
     $('#btnSignUp').click(() => {
