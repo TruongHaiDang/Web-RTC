17034900385fd7eb79b64c99cad08cedd38d305b
Truong Hai Dang <haidanghth910@gmail.com>
2020-05-06 16:22:22 +0700

    make it better

--- script/main.js
+++ script/main.js
@@ -1,13 +1,9 @@
 const client = io.connect("https://call-video-rtc.herokuapp.com", {secure: true});
 // const client = io('http://localhost:6969');
-const arrUsers = [];
-
+const arrRemote = [];
 client.on('signup_notifications', (data) => {
-    // for(i in data) {
-        $('#userName').val("");
-        $('#userInfo').append(`<li class="list-group-item" id="${data.id}" onclick="startStream('${data.id}')"> ${data.userName} </li>`)
-        flag = true
-    // }
+    $('#userName').val("");
+    $('#userInfo').append(`<li class="list-group-item" id="${data.id}" onclick="startStream('${data.id}')"> ${data.userName} </li>`)
 })
 
 client.on("Fail", () => alert("Đăng ký thất bại"))
@@ -15,7 +11,6 @@ client.on("Fail", () => alert("Đăng ký thất bại"))
 client.on('user_signout', (data) => {
     alert(`${data} disconnected`)
     $(`#${data}`).remove()
-    flag = false
 })
 
 client.on('load_video_section', () => $('.videoContent').show())
@@ -31,7 +26,6 @@ function playStream(idVideoTag, stream) {
     video.play();
 }
 
-// var peer = new Peer({host:'call-video-rtc.herokuapp.com', secure:true, port:11671});
 var peer = new Peer();
 peer.on('open', id => {
     $('#nameLocalSection').append(`<h3>My ID: ${id}</h3>`);
@@ -42,35 +36,46 @@ peer.on('open', id => {
 });
 
 $('document').ready(() => {
-    $('#btnCall').click(() => {
-        const id = $('#remoteId').val();
-        openStream()
-        .then(stream => {
-            playStream('localStream', stream);
-            const call = peer.call(id, stream);
-            call.on('stream', remoteStream => playStream('remoteStream', remoteStream))
-        }), (err) => {
-            console.log('Failed to get local stream' ,err);
-        }
-    })
+    // $('#btnCall').click(() => {
+    //     const id = $('#remoteId').val();
+    //     openStream()
+    //     .then(stream => {
+    //         playStream('localStream', stream);
+    //         const call = peer.call(id, stream);
+    //         call.on('stream', remoteStream => playStream('remoteStream', remoteStream))
+    //     }), (err) => {
+    //         console.log('Failed to get local stream' ,err);
+    //     }
+    // })
 
     $('.videoContent').hide()
 })
 
 peer.on('call', call => {
+    $('#container').append(`<div class="row">
+                                <div class="col-12 videoContent">
+                                    <video id="${call.peer + 'remote'}" controls></video>
+                                </div>
+                            </div>`)
     openStream()
     .then(stream => {
         call.answer(stream);
         playStream('localStream', stream);
-        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+        call.on('stream', remoteStream => playStream(call.peer + 'remote', remoteStream));
     })
 })
 
 function startStream(id){
+    $('#container').append(`<div class="row">
+                                <div class="col-12 videoContent">
+                                    <video id="${id + 'remote'}" controls></video>
+                                </div>
+                            </div>`)
+    arrRemote.push(id)
     openStream()
     .then(stream => {
         playStream('localStream', stream)
         const call = peer.call(id, stream)
-        call.on('stream', remoteStream => playStream('remoteStream', remoteStream))
+        call.on('stream', remoteStream => playStream(id + 'remote', remoteStream))
     })
 }
